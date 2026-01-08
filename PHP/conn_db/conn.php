3d6eb51b794bd198f9d947c884867bd236d92596
Ivan moreschi <moreschibrothers@gmail.com>
2026-01-08 10:20:29 +0000

    com

--- PHP/conn_db/conn.php
+++ PHP/conn_db/conn.php
@@ -25,27 +25,7 @@ if (isset($_POST['nome'], $_POST['cognome'], $_POST['data_nascita'], $_POST['ema
     }
 }
 
-// MEDIA >= 6
-echo "<h2>Studenti con media ≥ 6</h2>";
 
-$sql = "
-SELECT s.nome, s.cognome, AVG(v.voto) AS media
-FROM studenti s
-JOIN voti v ON v.matricola = s.matricola
-GROUP BY s.matricola, s.nome, s.cognome
-HAVING AVG(v.voto) >= 6
-";
-
-$result = $conn->query($sql);
-
-if (!$result) {
-    die("Errore SELECT: " . $conn->error);
-}
-
-while ($row = $result->fetch_assoc()) {
-    echo $row['nome'] . " " . $row['cognome'] .
-         " - Media: " . round($row['media'], 2) . "<br>";
-}
 
 $conn->close();
 ?>
